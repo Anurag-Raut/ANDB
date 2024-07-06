b93407ddfd90bb5c4d364b226aa191e90c69aeaa
Anurag Raut <rautab0@gmail.com>
2024-07-06 08:33:48 +0530

    first commit

---
+++ build/CMakeFiles/andb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/andb.dir/main.cpp.o"
+  "CMakeFiles/andb.dir/main.cpp.o.d"
+  "andb"
+  "andb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/andb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
